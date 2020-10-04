83db939f28f1f3837cf14cd970fcb2935030931a
Ruben S. Sevaldson <rubenss@stud.ntnu.no>
2020-10-04 17:12:11 +0200

    Formatting

--- js/canvas-helpers.js
+++ js/canvas-helpers.js
@@ -2,16 +2,24 @@ function onCanvasClick(canvas, color, coords, event) {
   let rect = canvas.getBoundingClientRect();
   let x = event.clientX - rect.left;
   let y = event.clientY - rect.top;
-  console.log((400 - y) - 200)
-  coords.push({ coords: [1.0, x - 200, (400 - y) - 200 ], color: color === 'blue' ? 1.0 : 0.0 });
+  console.log(400 - y - 200);
+  coords.push({
+    coords: [1.0, x - 200, 400 - y - 200],
+    color: color === "blue" ? 1.0 : 0.0,
+  });
 }
 
-
 function drawDots(context, coords) {
-  coords.forEach(coordObj => {
-    context.fillStyle = coordObj.color === 1.0 ? "#0000FF" : "#FF0000"
+  coords.forEach((coordObj) => {
+    context.fillStyle = coordObj.color === 1.0 ? "#0000FF" : "#FF0000";
     context.beginPath();
-    context.arc(coordObj.coords[1] + 200, (400 - (coordObj.coords[2] + 200)), 2, 0, 2 * Math.PI);
+    context.arc(
+      coordObj.coords[1] + 200,
+      400 - (coordObj.coords[2] + 200),
+      2,
+      0,
+      2 * Math.PI
+    );
     context.fill();
-  })
+  });
 }
--- js/perceptron.js
+++ js/perceptron.js
@@ -1,22 +1,18 @@
 function predict(coords, weights, threshold) {
-  const reducer = (accumulator, currentCoord, i) => accumulator + (currentCoord * weights[i]);
-  const result = coords.reduce(reducer, 0.0)
-  return result > threshold ? 1.0 : 0.0
+  const reducer = (accumulator, currentCoord, i) =>
+    accumulator + currentCoord * weights[i];
+  const result = coords.reduce(reducer, 0.0);
+  return result > threshold ? 1.0 : 0.0;
 }
-
-
 
 function trainWeights(numTimes, coordsArr, weights, l_rate, threshold) {
   for (let i = 0; i < numTimes; i++) {
-    coordsArr.forEach(coordsObj => {
-      const predicted = predict(coordsObj.coords, weights, threshold)
-      const error = coordsObj.color - predicted
+    coordsArr.forEach((coordsObj) => {
+      const predicted = predict(coordsObj.coords, weights, threshold);
+      const error = coordsObj.color - predicted;
       coordsObj.coords.forEach((coord, i) => {
-        weights[i] += error * l_rate * coord
-      })
-
-    })
+        weights[i] += error * l_rate * coord;
+      });
+    });
   }
 }
-
-
